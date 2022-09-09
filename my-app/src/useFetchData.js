617a372bc4f742ed9457733c88a340e1fa0d33bf
MOUSHUMI DAS <moushumi.das.phy@gmail.com>
2022-09-09 14:13:29 -0500

    Create useFetchData.js

---
+++ my-app/src/useFetchData.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(url,{method: 'GET'});
+        const result = await res.json();
+        setData(result.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
+
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(url,{method: 'GET'});
+        const result = await res.json();
+        setData(result.data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  return { data, loading, error, reFetch };
+};
+
+export default useFetch;
